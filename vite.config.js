cfc35f3cbee8dc4bf0f03041eb46efa3d7e4f751
Mel Canis <mel.canis353@gmail.com>
2024-02-03 10:31:03 +0900

    imagine

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5500',
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const PORT = env.PORT || "http://localhost:5500"
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: PORT,
+          changeOrigin: true,
+          secure: false
+        }
       }
     }
   }
